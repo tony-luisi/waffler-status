2ba80428200b3dd2f7983edf9e4578c832af92a6
tony-luisi <t.luisi@gmail.com>
2016-07-19 08:41:49 +1200

    established connection with token

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+require('dotenv').config()
+var request = require('superagent')
+
+
 //get all the issues from a repo for a specific cohort
+var endpoint = 'https://api.github.com'
 
-
+request
+  .get(endpoint + '/user/repos')
+  .set('Authorization', 'token ' + process.env.GITHUB_TOKEN)
+  .end(function(req, res) {
+    console.log(res.body)
+  })
 
 //group by label - user / sprint / badge
 
